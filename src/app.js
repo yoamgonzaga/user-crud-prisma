f1cca206d1f9d49f449b8223e2b1b87040af6b1a
Orlando González <yoamgonzaga@gmail.com>
2026-04-15 22:15:50 -0600

    enable cors

--- src/app.js
+++ src/app.js
@@ -5,8 +5,16 @@ const errorHandler = require("./middlewares/errorHandler");
 
 const userRoutes = require("./routes/userRoutes");
 const postRoutes = require("./routes/postRoutes");
+const cors = require("cors");
 
 app.use(express.json()); // importante para JSON
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.use("/api", userRoutes);
 app.use("/api", postRoutes);
