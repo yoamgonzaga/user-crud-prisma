a3a36485d00f97252276b6b96ca4a55dc89a262b
Orlando González <yoamgonzaga@gmail.com>
2026-04-21 19:16:51 -0600

    fix: enable cors

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 app.use(express.json()); // importante para JSON
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
